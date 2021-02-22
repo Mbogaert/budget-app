4b55615fa30b844fe05715130a267a2212a8d036
Mathew B <bogaert@email.arizona.edu>
2021-02-21 21:42:34 -0700

    fixed typos, manifest working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,61 +1,62 @@
-const APP_PREFIX = 'BudgetApp-';
-const VERSION = 'version_01';
+const APP_PREFIX = "BudgetApp-";
+const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "/",
-    "/icons.icon-72x72.png",
-    "/icons/icon-96x96.png",
-    "/icons/icon-128x128.png",
-    "/icons/icon-144x144.png",
-    "/icons/icon-152x152.png",
-    "/icons/icon-192x192.png",
-    "/icons/icon-384x384.png",
-    "/icons/icon-512x512.png",
-    "/css/styles.cc",
-    "/index.html",
-    "/js/index.js",
-    "/manifest.json"
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+  "./css/styles.css",
+  "./index.html",
+  "./js/idb.js",
+  "./js/index.js",
+  "./manifest.json",
 ];
+
+// installing the service-worker
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// activate service-workers
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
 
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
         caches.match(e.request).then(function (request) {
             if (request) {
-              console.log('responding with cache : ' + e.request.url)
-              return request
+                console.log('responding with cache : ' + e.request.url)
+                return request
             } else {
                 console.log('file is not cached, fetching : ' + e.request.url)
                 return fetch(e.request)
             }
-          })
-    )
-  })
 
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
         })
-    )
-});
 
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (ketList) {
-            let cacheKeeplist = ketList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            })
-            cacheKeeplist.push(CACHE_NAME);
-
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheKeeplist.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i] );
-                    return caches.delete(keyList[i]);
-                }
-            }));
-        })
     )
 })
